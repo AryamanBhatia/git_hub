f6ffac9f1e4290ed3c237dac75a6179da2967ab6
AryamanBhatia <arycodeswell@gmail.com>
2022-08-16 13:09:23 +0530

    Initial Commit

---
+++ git_hub/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  git_hub
+//
+//  Created by Aryaman Bhatia on 16/08/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
